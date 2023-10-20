ae388ec839975a0a1277eae9d4a4203810a01536
Park Jihyun <129791089+lucihyun@users.noreply.github.com>
2023-10-21 00:06:27 +0900

    [Bronze V] Title: 나머지, Time: 212 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		if(a>=2 && b>=2 && c>=2 && a<=10000 && b<=10000 && c<=10000) {
+			System.out.println((a+b)%c);
+			System.out.println( ((a%c) + (b%c))%c);
+			System.out.println((a*b)%c);
+			System.out.println( ((a%c)*(b%c))%c );
+		}
+	}
+}
